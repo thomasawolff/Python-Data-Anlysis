d5dc5ae50a52c8c461ab5be8a94f65b5f3fd91f3
Thomas Wolff <twolff@mt.gov>
2017-10-05 09:25:20 -0600

    Add files via upload

--- driveScanSearch.py
+++ driveScanSearch.py
@@ -7,15 +7,22 @@ import time
 drives internal and external and searches for
 prft and PRFT files (or any other kind of file)'''
 
-driveSet = ['A:','B:','D:','E:','F:','G:',\
-            'H:','I:','J:','K:','L:','M:',\
-            'N:','O:','P:','Q:','R:','S:',\
-            'T:','U:','V:','W:','X:','Y:','Z:']
+##driveSet = ['A:','B:','D:','E:','F:','G:',\
+##            'H:','I:','J:','K:','L:','M:',\
+##            'N:','O:','P:','Q:','R:','S:',\
+##            'T:','U:','V:','W:','X:','Y:','Z:']
+
+
+driveSet = ['A:','B:','D:','F:','I:','J:','K:','L:','M:',\
+            'N:','O:','P:','S:','U:','V:']
+
 
 def driveFind():
     for drives in driveSet:
         if os.path.isdir(drives)==True: # letters from driveSet
             yield drives                # that are drives are yielded
+        #else: print 'No drives'
+driveFind()
             
 def setFinder():
     for driveList in driveFind():
@@ -23,8 +30,8 @@ def setFinder():
         except WindowsError:pass # pass by the drive if error
         print 'Drive being scanned:',driveList
         for setFolder in os.listdir(os.getcwd()): # for folder in list of folders
-            if os.path.isdir(setFolder)==True and\ 
-               len(setFolder)==3 and\
+            if os.path.isdir(setFolder)==True and \
+               len(setFolder)==3 and \
                setFolder.endswith('.sec')==False: # if it ends with '.sec'
                 try: type(int(setFolder[-3:])) # if the folder name ends with 3 numbers
                 except ValueError:continue # code above will raise error if false: continue
@@ -32,27 +39,48 @@ def setFinder():
         if driveList == driveList[-1]: # when it gets to end of driveList stop
             sys.exit(0) # exit from the code
 
-def missingFileSearch(): # searches for prft files
+def missingFileSearchPRFT(): # searches for prft files
     for line in setFinder():
         data = os.listdir(line)
-        if('prft0002.'+line[-3:] in data):
-            pass
-        elif('PRFT0002.'+line[-3:] in data):
-            pass
+        if('prft0002.'+line[-3:] in data) or ('PRFT0002.'+line[-3:] in data):
+            print'Fine here:',line
         else:
-            yield 'PRFT Data is missing here:',line # tells you where data is missing
-missingFileSearch()
+            print 'PRFT Data is missing here:',line # tells you where data is missing
+#missingFileSearchPRFT()
+
+def missingFileSearchBoxes(): # searches for area crack and boxes files
+    setList = []
+    for line in setFinder():
+        os.chdir(line)
+        folder = os.listdir(line)
+        for minFolder in folder:
+            if os.path.isdir(minFolder)==True:
+                for data in os.listdir(minFolder):
+                    if data == str(data[0:12])+'_area_boxes_cr.txt' or \
+                       data == str(data[0:12])+'_area_pixels_cr.txt' or \
+                       data == str(data[0:12])+'_crack_pixels_cr.txt' or \
+                       data == str(data[0:12])+'_boxes_cr.txt' or \
+                       data == str(data[0:12])+'.jpg' or \
+                       data == str(data[0:12])+'.3dc' or \
+                       data == str(data[0:12])+'.jpg.thmb' or \
+                       data == str(data[0:12])+'_intensity.jpg' or \
+                       data == str(data[0:12])+'_intensity.jpg.thmb' or \
+                       data == str(data[0:12])+'_depth.jpg' or \
+                       data == str(data[0:12])+'_depth.jpg.thmb' or \
+                       data == 'Errors.txt' or \
+                       data == 'Thumbs.db': pass
+                    else: print line,minFolder,data
 
 
 def dataYield(): # writes output to a csv file
     os.chdir('C:\\\\Users\\\\U2970\\\\Desktop')
     with open('missingDataPrft.csv','w') as fp:
-        for line in missingFileSearch():
+        for line in missingFileSearchPRFT():
             print line
             filename = csv.writer(fp)
             filename.writerow(line)
             
 start_time = time.time()
-dataYield()
+missingFileSearchBoxes()
 print("--- %s seconds ---" % (time.time() - start_time))
 
