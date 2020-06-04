7e5d833ec54a0d58fcfa26823343516e80e950f5
Thomas Wolff <datawolff@gmail.com>
2020-06-04 08:10:24 -0600

    Add files via upload

---
+++ pdfMinerCode.py
@@ -0,0 +1,33 @@
+
+import io
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, 
+                                  caching=caching,
+                                  check_extractable=True):
+        interpreter.process_page(page)
+
+    fp.close()
+    device.close()
+    text = retstr.getvalue()
+    retstr.close()
+    return text
+
+path = '\\\\hhs-hlnshare\\\\shared\\\\phs\MTmechv Data\\\\Silvana and Tom Data\\\\Data in Motion_January 2018.pdf'
+print(convert_pdf_to_txt(path))
